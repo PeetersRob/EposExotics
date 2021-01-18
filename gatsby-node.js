f630b174ff3328da0dc845326584bf656deac39f
Rob Peeters <s099271@ap.be>
2021-01-19 00:36:16 +0100

    query voor data cars te halen vaan dynamic pages aan te maken

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,4 +1,29 @@
+const { graphql } = require("gatsby")
 const { createRemoteFileNode } = require(`gatsby-source-filesystem`)
+
+exports.createPages = ({graphql, actions}) => {
+    const query = graphql(`
+        {
+            wpcontent {
+                cars {
+                    edges {
+                        node {
+                            slug
+                            id
+                        }
+                    }
+                }
+            }
+        }
+    `).then(result => {
+        if (result.errors) {
+            result.errors.forEach(e => console.log(e.toString()))
+            return Promise.reject(result.errors);
+        }
+        const cars = result.data.wpcontent.cars.edges;
+        console.log(cars, "console log cars data")
+    })
+}
 
 /* Aan de hand van dit stukje code worden de images vanuit WPgraphql omgezet tot images waarop Gatsby image optimization kan toepassen */
 exports.createResolvers = async ({
