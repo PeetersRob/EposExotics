6dba9695af604c6d3f34aaea414af3791f2779d7
Rob Peeters <s099271@ap.be>
2021-01-22 22:07:50 +0100

    Build errors opgelost, preformance aanpassingen voor lighthouse score

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import { useStaticQuery, graphql } from "gatsby"
 
 import Layout from "../components/Layout"
 import SEO from "../components/Seo"
-import {Wrapper, Image, Car} from "./pageStyles/pageStyles"
+import {Wrapper, Image, Car} from "../pageStyles/pageStyles"
 
 const IndexPage = () => {
   const {
