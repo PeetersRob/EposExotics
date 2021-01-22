a864e76ce23a0362d1c1a25a71f73cb9f8fec2ff
Rob Peeters <s099271@ap.be>
2021-01-22 22:39:57 +0100

    aanpassingen voor netlify

--- src/templates/car.js
+++ src/templates/car.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import {graphql} from 'gatsby'
 
-import Layout from '../components/Layout'
-import SEO from '../components/Seo'
+import Layout from '../components/layout'
+import SEO from '../components/seo'
 import {Wrapper, Image} from './templatesStyles/carStyle'
 
 const Artist = ({data: {wpcontent: {car: {car, makes: {edges: makes}}}}}) => {
