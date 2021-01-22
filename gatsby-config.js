a92a1e84b94977d7a6db8c38d2af66c966d9ddca
Rob Peeters <s099271@ap.be>
2021-01-22 20:15:16 +0100

    Aanpassingen SiteMetaData

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Epos Exotic Cars`,
+    description: `Epos Exotics is the place to go for rare and exlusive cars`,
+    author: `@Rob Peeters`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
